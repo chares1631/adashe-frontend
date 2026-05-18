1893f92f0243ebc5efe781e3a8f37d10f0d9bbde
chares1631 <charlessylvanus11@gmail.com>
2026-05-18 09:32:19 +0100

    fix ABI to JSON format

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -6,13 +6,13 @@ export const CONTRACTS = {
 } as const
 
 export const ADASHE_ABI = [
-  'function createGroup(uint256,uint256,uint256) returns (uint256)',
-  'function joinGroup(uint256)',
-  'function contribute(uint256)',
-  'function getGroup(uint256) view returns (address,uint256,uint256,uint256,uint256,uint256,bool)',
-  'function getMembers(uint256) view returns (address[])',
-  'function hasContributed(uint256,address) view returns (bool)',
-  'function groupCount() view returns (uint256)',
+  { name: 'createGroup', type: 'function', stateMutability: 'nonpayable', inputs: [{ name: 'amount', type: 'uint256' }, { name: 'cycleDuration', type: 'uint256' }, { name: 'maxMembers', type: 'uint256' }], outputs: [{ name: '', type: 'uint256' }] },
+  { name: 'joinGroup', type: 'function', stateMutability: 'nonpayable', inputs: [{ name: 'groupId', type: 'uint256' }], outputs: [] },
+  { name: 'contribute', type: 'function', stateMutability: 'nonpayable', inputs: [{ name: 'groupId', type: 'uint256' }], outputs: [] },
+  { name: 'getGroup', type: 'function', stateMutability: 'view', inputs: [{ name: 'groupId', type: 'uint256' }], outputs: [{ name: '', type: 'address' }, { name: '', type: 'uint256' }, { name: '', type: 'uint256' }, { name: '', type: 'uint256' }, { name: '', type: 'uint256' }, { name: '', type: 'uint256' }, { name: '', type: 'bool' }] },
+  { name: 'getMembers', type: 'function', stateMutability: 'view', inputs: [{ name: 'groupId', type: 'uint256' }], outputs: [{ name: '', type: 'address[]' }] },
+  { name: 'hasContributed', type: 'function', stateMutability: 'view', inputs: [{ name: 'groupId', type: 'uint256' }, { name: 'member', type: 'address' }], outputs: [{ name: '', type: 'bool' }] },
+  { name: 'groupCount', type: 'function', stateMutability: 'view', inputs: [], outputs: [{ name: '', type: 'uint256' }] },
 ] as const
 
 export const ERC20_ABI = [
